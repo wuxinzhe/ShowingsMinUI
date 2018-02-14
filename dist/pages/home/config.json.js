4a479ea8d755b99fd1f4e2a07c87382a87a1afd1
wuxinzhe@126.com <wuxinzhe@126.com>
2018-02-14 22:35:31 +0800

    添加组件input

--- dist/pages/home/config.json.js
+++ dist/pages/home/config.json.js
@@ -7,6 +7,12 @@ module.exports = {
       "icon": "https://s10.mogucdn.com/mlcdn/c45406/171122_7al50lach13ji6jggj63d17h522dh_200x200.png",
       "pages": [
         {
+          "id": "input",
+          "name": "文本输入",
+          "icon": "",
+          "code": ""
+        },
+        {
           "id": "list",
           "name": "列表",
           "icon": "",
